40c29f5c28bb7f289301540a9a7234fdca40055d
Rock Clapps <rock@secureco.com>
2024-02-09 21:45:28 -0500

    Added logit to delete old files if disk space is needed and cycle to new files if they are too big

--- tools.go
+++ tools.go
@@ -2,15 +2,38 @@ package loge
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
+	"sort"
 	"time"
 )
 
 const dateTimeStringLength = 27
+const maxFileSize = 500
 
 func getLogName(path string) string {
+	fileList, _ := os.ReadDir(path)
+	filesExist := len(fileList) > 0
+	if filesExist {
+		sort.Slice(fileList,
+			func(x int, y int) bool {
+				return fileList[x].Name() < fileList[y].Name()
+			})
+	}
 	t := time.Now()
-	ret := fmt.Sprintf("%d%02d%02d.log", t.Year(), t.Month(), t.Day())
+	if filesExist {
+		dateStr := fmt.Sprintf("%d%02d%02d", t.Year(), t.Month(), t.Day())
+		if fileList[0].Name()[:dateTimeStringLength] != dateStr {
+			return filepath.Join(path, dateStr+"_1.log")
+		}
+	}
+	fileNum := int(fileList[0].Name()[dateTimeStringLength+2])
+	ret := fmt.Sprintf("%d%02d%02d_%d.log", t.Year(), t.Month(), t.Day(), fileNum)
+
+	if getFileSize(filepath.Join(path, ret)) > maxFileSize {
+		ret = fmt.Sprintf("%d%02d%02d_%d.log", t.Year(), t.Month(), t.Day(), fileNum+1)
+	}
+
 	return filepath.Join(path, ret)
 }
 
@@ -47,4 +70,18 @@ func dumpTimeToBuffer(buf *[]byte, t time.Time) {
 	*buf = append(*buf, '.')
 	itoa(buf, t.Nanosecond()/1e3, 6)
 	*buf = append(*buf, ' ')
+}
+
+func getFileSize(file string) int64 {
+	var size int64
+	size = -1
+	f, err := os.Open(file)
+	if err != nil {
+		return size
+	}
+	defer f.Close()
+
+	fInfo, _ := f.Stat()
+	size = fInfo.Size()
+	return size
 }
